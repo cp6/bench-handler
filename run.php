c26c61a304fe0ec8a77bc69e21993731339f453f
cp6 <cp6@users.noreply.github.com>
2018-07-25 12:57:37 +1000

    Updated to reflect real usage

--- run.php
+++ run.php
@@ -1,3 +1,8 @@
 <?php
-//echo json_encode(benchsh('vps_small_la', 'bench.log', 2));
-//echo json_encode(nench('vps_small_la', 'nench.log', 2));
+require('functions.php');
+
+echo benchsh('1gb_la_vultr', 'thebench.log', 1);//for MYSQL INSERT
+//echo json_encode(benchsh('server_name', 'thebench.log', 2));//for JSON output
+
+//echo nench('512mb_la', 'thenench.log', 1);//for MYSQL INSERT
+//echo json_encode(nench('512mb_la', 'thenench.log', 2));//for JSON output
